a333cdc28cd7ecd12fd503353fc36c72460c5b91
Pareekshithan08 <pareekshithan08@gmail.com>
2026-03-10 09:29:15 +0530

    error

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -9,3 +9,7 @@ output "cluster_endpoint" {
 output "cluster_certificate_authority_data" {
   value = aws_eks_cluster.eks.certificate_authority[0].data
 }
+
+output "lb_controller_role_arn" {
+  value = aws_iam_role.aws_load_balancer_controller.arn
+}
